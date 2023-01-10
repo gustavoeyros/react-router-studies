89001033ea2f2039e5b87ce3580751449f8a442f
Gustavo Eyros <gustavoeyros@hotmail.com>
2023-01-10 17:04:20 -0300

    feat: switch and dynamic routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import "./App.css";
 import Welcome from "./pages/Welcome";
 import Products from "./pages/Products";
 import MainHeader from "./components/MainHeader";
+import ProductDetail from "./pages/ProductDetail";
 function App() {
   return (
     <div>
@@ -10,13 +11,19 @@ function App() {
         <MainHeader />
       </header>
       <main>
-        <Route path="/welcome">
-          <Welcome />
-        </Route>
+        <Switch>
+          <Route path="/welcome">
+            <Welcome />
+          </Route>
 
-        <Route path="/products">
-          <Products />
-        </Route>
+          <Route path="/products" exact>
+            <Products />
+          </Route>
+
+          <Route path="/products/:productId">
+            <ProductDetail />
+          </Route>
+        </Switch>
       </main>
     </div>
   );
