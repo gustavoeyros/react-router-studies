3ba37c407cba244d97af9de5e2b37418ebb60483
Gustavo Eyros <gustavoeyros@hotmail.com>
2023-01-11 12:11:23 -0300

    feat: second project setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,9 @@
-import { Route, Switch, Redirect } from "react-router-dom";
 import "./App.css";
-import Welcome from "./pages/Welcome";
-import Products from "./pages/Products";
-import MainHeader from "./components/MainHeader";
-import ProductDetail from "./pages/ProductDetail";
+
 function App() {
   return (
     <div>
-      <header>
-        <MainHeader />
-      </header>
-      <main>
-        <Switch>
-          <Route path="/" exact>
-            <Redirect to="/welcome" />
-          </Route>
-
-          <Route path="/welcome">
-            <Welcome />
-          </Route>
-
-          <Route path="/products" exact>
-            <Products />
-          </Route>
-
-          <Route path="/products/:productId">
-            <ProductDetail />
-          </Route>
-        </Switch>
-      </main>
+      <h1>Olá!</h1>
     </div>
   );
 }
