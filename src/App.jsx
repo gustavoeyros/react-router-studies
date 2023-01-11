a67922b05d48f857abe730b36fd07da87d10a4a5
Gustavo Eyros <gustavoeyros@hotmail.com>
2023-01-11 13:47:16 -0300

    feat: routes definition

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
 import "./App.css";
+import { Route, Switch } from "react-router-dom";
+import AllQuotes from "./pages/AllQuotes";
+import NewQuote from "./pages/NewQuote";
+import QuoteDetail from "./pages/QuoteDetail";
 
 function App() {
   return (
-    <div>
-      <h1>Olá!</h1>
-    </div>
+    <Switch>
+      <Route path="/quotes">
+        <AllQuotes />
+      </Route>
+      <Route path="/quotes/:quoteId">
+        <QuoteDetail />
+      </Route>
+      <Route path="/new-quote">
+        <NewQuote />
+      </Route>
+    </Switch>
   );
 }
 
