1e91fdfb69b1e00c0f7493310b7f066aec234f4b
Gustavo Eyros <gustavoeyros@hotmail.com>
2023-01-11 14:19:00 -0300

    feat: details page

--- src/pages/AllQuotes.jsx
+++ src/pages/AllQuotes.jsx
@@ -1,5 +1,20 @@
+import QuoteList from "../components/quotes/QuoteList";
+
+const DUMMY_QUOTES = [
+  {
+    id: "q1",
+    author: "Gus",
+    text: "React is fun!",
+  },
+  {
+    id: "q2",
+    author: "Gustavo",
+    text: "React is great!",
+  },
+];
+
 const AllQuotes = () => {
-  return <h1>All Quotes Page</h1>;
+  return <QuoteList quotes={DUMMY_QUOTES} />;
 };
 
 export default AllQuotes;
